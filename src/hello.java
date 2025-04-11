0d21104454bf12421c452d01047eaa913d9e3078
pavangajam <pavangajam1234@gmail.com>
2025-04-12 00:29:37 +0530

    [test-3]Modified hello file

--- src/hello.java
+++ src/hello.java
@@ -1,2 +1,31 @@
 public class hello {
+    private String message;
+    private int number;
+
+    // Constructor
+    public hello(String message, int number) {
+        this.message = message;
+        this.number = number;
+    }
+
+    // Getter methods
+    public String getMessage() {
+        return message;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    // Display method
+    public void displayInfo() {
+        System.out.println("Message: " + message);
+        System.out.println("Number: " + number);
+    }
+
+    // Main method for testing
+    public static void main(String[] args) {
+        hello obj = new hello("Hello from Java!", 42);
+        obj.displayInfo();
+    }
 }
