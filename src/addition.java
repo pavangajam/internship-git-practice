fd24e64f04ce97dc784d7d9acea3405d1e3b819c
pavangajam <pavangajam1234@gmail.com>
2025-04-12 00:56:40 +0530

    [test-6]Added functionality to addition file

--- src/addition.java
+++ src/addition.java
@@ -1,2 +1,22 @@
 public class addition {
+    private int a;
+    private int b;
+
+    public addition(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int add() {
+        return a + b;
+    }
+
+    public void displayResult() {
+        System.out.println("The sum of " + a + " and " + b + " is: " + add());
+    }
+
+    public static void main(String[] args) {
+        addition obj = new addition(10, 20);
+        obj.displayResult();
+    }
 }
