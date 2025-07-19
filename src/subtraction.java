2bfbd8a84893bec44f7daf10829eba89249756ef
pavangajam <pavangajam1234@gmail.com>
2025-07-19 22:40:02 +0530

    [PS-212]: Modify subtraction file

--- src/subtraction.java
+++ src/subtraction.java
@@ -1,11 +1,20 @@
 public class subtraction {
     private String message;
     private int number;
+    private int subtractFrom; // new field
 
     // Constructor
     public subtraction(String message, int number) {
         this.message = message;
         this.number = number;
+        this.subtractFrom = 0; // default value
+    }
+
+    // Overloaded constructor to set subtractFrom value
+    public subtraction(String message, int number, int subtractFrom) {
+        this.message = message;
+        this.number = number;
+        this.subtractFrom = subtractFrom;
     }
 
     // Getter methods
@@ -17,15 +26,42 @@ public class subtraction {
         return number;
     }
 
+    public int getSubtractFrom() {
+        return subtractFrom;
+    }
+
+    // Method to perform subtraction
+    public int performSubtraction() {
+        int result = subtractFrom - number;
+        if (result < 0) {
+            System.out.println("Warning: Result is negative.");
+        }
+        return result;
+    }
+
+    // Method to update values
+    public void updateInfo(String newMessage, int newNumber, int newSubtractFrom) {
+        this.message = newMessage;
+        this.number = newNumber;
+        this.subtractFrom = newSubtractFrom;
+    }
+
     // Display method
     public void displayInfo() {
         System.out.println("Message: " + message);
         System.out.println("Number: " + number);
+        System.out.println("Subtracting " + number + " from " + subtractFrom);
+        System.out.println("Result: " + performSubtraction());
     }
 
     // Main method for testing
     public static void main(String[] args) {
-        hello obj = new hello("New file addition",2);
+        subtraction obj = new subtraction("Performing subtraction", 5, 10);
+        obj.displayInfo();
+
+        // Updating values
+        obj.updateInfo("Updated subtraction", 7, 20);
+        System.out.println("\nAfter update:");
         obj.displayInfo();
     }
 }
