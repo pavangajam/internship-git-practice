d845a874228747e77014ec3c617343fc497f9117
pavangajam <pavangajam1234@gmail.com>
2025-07-20 00:14:14 +0530

    [PS-216]: ADD Division file

---
+++ src/division.java
@@ -0,0 +1,45 @@
+public class division {
+    private double a;
+    private double b;
+
+    // Constructor
+    public division(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    // Method to perform division
+    public double divide() {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero.");
+        }
+        return a / b;
+    }
+
+    // Method to update values
+    public void updateValues(double newA, double newB) {
+        this.a = newA;
+        this.b = newB;
+    }
+
+    // Display method
+    public void displayResult() {
+        System.out.println("Dividing " + a + " by " + b);
+        try {
+            System.out.println("Result: " + divide());
+        } catch (ArithmeticException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    // Main method for testing
+    public static void main(String[] args) {
+        division obj = new division(10, 2);
+        obj.displayResult();
+
+        // Try dividing by zero
+        obj.updateValues(10, 0);
+        System.out.println("\nAfter updating values:");
+        obj.displayResult();
+    }
+}
