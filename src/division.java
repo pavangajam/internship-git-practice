1d0045505d59632d6bbf20f1c8581acd92dc7a5b
pavangajam <pavangajam1234@gmail.com>
2025-10-04 10:01:32 +0530

    [PS-218]: update division-file-2 (conflicting case)

--- src/division.java
+++ src/division.java
@@ -27,6 +27,7 @@ public class division {
         System.out.println("Dividing " + a + " by " + b);
         try {
             System.out.println("Result: " + divide());
+            System.out.println("conflicting change!!");
         } catch (ArithmeticException e) {
             System.out.println("Error: " + e.getMessage());
         }
